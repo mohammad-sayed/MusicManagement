cc3639b600f0cc2343c61d86216dc6bcac806ea1
Mohammad Sayed <ms@next-munich.com>
2022-12-04 18:33:11 +0100

    Add missing LastFMRetrofitService

---
+++ app/src/main/java/com/ms/musicmanagement/shared/datasource/backend/LastFMRetrofitService.kt
@@ -0,0 +1,10 @@
+package com.ms.musicmanagement.shared.datasource.backend
+
+import com.ms.musicmanagement.shared.datasource.backend.constant.LastFMApiParameters
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface LastFMRetrofitService {
+}
